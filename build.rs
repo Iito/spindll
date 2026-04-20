1437495cee05c8256569dc17cfed1c0130addf71
Iito <mansa.camara.jp@gmail.com>
2026-04-20 23:27:47 +0900

    refactor: gRPC is default library, cli feature is binary-only extras

--- build.rs
+++ build.rs
@@ -1,10 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Only compile protobuf when the `grpc` feature is enabled.
-    #[cfg(feature = "cli")]
-    {
-        println!("cargo:rerun-if-changed=proto/spindll.proto");
-        println!("cargo:rerun-if-changed=proto");
-        tonic_build::compile_protos("proto/spindll.proto")?;
-    }
+    println!("cargo:rerun-if-changed=proto/spindll.proto");
+    println!("cargo:rerun-if-changed=proto");
+    tonic_build::compile_protos("proto/spindll.proto")?;
     Ok(())
 }
